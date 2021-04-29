39d6d25ffc24803d9e2db0c6cdfdfd674fcc8cde
Roy Shtoyerman <67565840+royee24@users.noreply.github.com>
2021-04-29 12:28:30 +0300

    Create linear_model.py

---
+++ linear_model.py
@@ -0,0 +1,39 @@
+from numpy import linalg
+import numpy as np
+
+
+def fit_linear_regression(X, y):
+    """
+
+    :param X:
+    :param y:
+    :return:
+    """
+    pass
+
+
+
+def mse(response_vector, prediction_vector):
+    """
+
+    :param response_vector:
+    :param prediction_vector:
+    :return:
+    """
+    pass
+
+
+
+
+def load_data(path):
+    """
+
+    :param path:
+    :return:
+    """
+    
+
+
+if __name__ == "__main__":
+    pass
+
